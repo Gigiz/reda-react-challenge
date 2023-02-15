851b7f3eb2d5762ed8dc59179ed4ad15442bec65
Luigi Colombi <lucolombi89@gmail.com>
2023-02-15 21:50:09 +0100

    chore(deps-dev): add vanilla-extract

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import { createRoot } from "react-dom/client"
 import { StrictMode } from "react"
 
+import { hello } from "styles.css"
+
 const container = document.getElementById("root")
 
 if (container) {
   const root = createRoot(container)
   root.render(
     <StrictMode>
-      <div>Hello Vite!</div>
+      <div className={hello}>Hello Vite!</div>
     </StrictMode>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: "./env",
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), vanillaExtractPlugin({ identifiers: 'short' }), tsconfigPaths()],
   build: {
     sourcemap: true,
   },
